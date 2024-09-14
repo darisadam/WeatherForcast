2564bfa21856a7ff82ce908b8840cb120911f6fa
damdaris <darisadam@upi.edu>
2024-09-14 22:36:02 +0700

    Initial Commit

---
+++ WeatherForcast/WeatherForcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcastApp.swift
+//  WeatherForcast
+//
+//  Created by Adam Daris Ryadhi on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
